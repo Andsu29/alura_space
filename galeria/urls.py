7530744acb0cc4c338cd28f1825df19bad9e30d0
Andsu <andersonduarteg4@gmail.com>
2024-04-07 10:02:07 -0300

    Configuração do Django Admin

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,6 +3,6 @@ from galeria.views import index, imagem, mais
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('mais/', mais, name='mais'),
 ]
